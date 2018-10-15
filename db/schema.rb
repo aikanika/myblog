9f20e4c6ff822280010b67c412b36065b61cc074
vagrant <vagrant@localhost.localdomain>
2018-10-16 07:26:49 +0900

    heroku commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_095942) do
+ActiveRecord::Schema.define(version: 2018_10_07_113636) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.text "name"
@@ -20,8 +23,8 @@ ActiveRecord::Schema.define(version: 2018_10_07_095942) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
-    t.integer "post_id"
-    t.integer "category_id"
+    t.bigint "post_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_comments_on_category_id"
@@ -31,7 +34,7 @@ ActiveRecord::Schema.define(version: 2018_10_07_095942) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "category_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_posts_on_category_id"
@@ -46,4 +49,7 @@ ActiveRecord::Schema.define(version: 2018_10_07_095942) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "categories"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "categories"
 end
